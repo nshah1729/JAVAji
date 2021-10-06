2e88a68b5079356935f3b47a6b36aa8877f918bf
nishantshah <nishantshah1729@gmail.com>
2021-10-06 19:17:58 +0530

    commit JAVA

---
+++ com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String s="";
+        for (int i=0;i<26;i++)
+        {
+            s+=(char)('a'+i);
+        }
+        System.out.println(s);
+    }
+}
---
+++ com/company/Recursion.java
@@ -0,0 +1,22 @@
+package com.company;
+import java.util.*;
+public class Recursion {
+    public static void main(String[] args) {
+        int[][] ar={
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+    };
+    int t=2;
+        System.out.println(Arrays.toString(bin(ar,t)));
+    }
+    static int[] bin(int[][] a,int target){
+        int r=0,c=a.length-1;
+        while(r<a.length&&c>=0){
+           if(a[r][c]==target)return new int[]{r,c};
+           else if(a[r][c]>target)c--;
+           else{r++;}
+        }
+        return new int[]{-1,-1};
+    }
+}
