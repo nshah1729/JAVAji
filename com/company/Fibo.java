6edef298b87677681ca69325c7382857cca4ae35
nishantshah <nishantshah1729@gmail.com>
2021-10-07 11:31:55 +0530

    commit JAVA

--- com/company/Fibo.java
+++ com/company/Fibo.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Fibo {
     public static void main(String[] args) {
-        System.out.println(fib(5));
+        System.out.println(fib(47)); //giving correct values till n=47
     }
     static int fib(int n){
         int a=0,b=1,count=0;
