4b6dc43fde5e5f27e228d1321f30ee13f26228b3
nishantshah <nishantshah1729@gmail.com>
2021-10-07 19:21:44 +0530

    commit JAVA

---
+++ com/company/Two_Sum.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Two_Sum {
+    public static void main(String[] args) {
+     int[] p={1,2,3,4,5};
+     int t=9;
+
+        System.out.println(Arrays.toString(find(p,t)));
+    }
+    static int[] find(int[] n, int t){
+        int s=0,e=n.length-1;
+        while(s<e){
+            if((n[s]+n[e])==t)break;
+            else if((n[s]+n[e])>t)e--;
+            else{s++;}
+        }
+        return new int[]{s+1,e+1};
+    }
+}
