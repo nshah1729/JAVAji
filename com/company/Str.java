d577d3fb0b5f3d5481747b0b798e73c95ca0856d
nishantshah <nishantshah1729@gmail.com>
2021-10-12 13:42:18 +0530

    commit JAVA

--- com/company/Str.java
+++ com/company/Str.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Str {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
+        int a = sc. nextInt();
         int b = sc.nextInt();
         int count = 0;
         for (int i = 1; i < Math.min(a, b); i++) {
