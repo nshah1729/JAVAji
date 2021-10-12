2f12b361c66810b6559f921c10a7ccf3cee79c26
nishantshah <nishantshah1729@gmail.com>
2021-10-12 19:53:47 +0530

    commit JAVA

--- com/company/Str.java
+++ com/company/Str.java
@@ -7,10 +7,10 @@ public class Str {
         Scanner sc = new Scanner(System.in);
         int a = sc. nextInt();
         int b = sc.nextInt();
-        int c = 0;
+        int count = 0;
         for (int i = 1; i < Math.min(a, b); i++) {
-            if ((a % i == 0) && (b % i == 0)) c++;
+            if ((a % i == 0) && (b % i == 0)) count++;
         }
-        System.out.println(c);
+        System.out.println(count);
     }
 }
