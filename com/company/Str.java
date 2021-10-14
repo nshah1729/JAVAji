543b1e8e2c0cada1a298822227d190d80287ffc3
nishantshah <nishantshah1729@gmail.com>
2021-10-14 09:52:26 +0530

    commit JAVA

--- com/company/Str.java
+++ com/company/Str.java
@@ -5,12 +5,22 @@ import java.util.*;
 public class Str {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc. nextInt();
-        int b = sc.nextInt();
-        int count = 0;
-        for (int i = 1; i < Math.min(a, b); i++) {
-            if ((a % i == 0) && (b % i == 0)) count++;
+        int[] a = new int[5];
+        System.out.println("Enter the elements");
+        for (int i = 0; i <5; i++) {
+            a[i]=sc.nextInt();
         }
-        System.out.println(count);
+        rev(a);
+        System.out.println(Arrays.toString(a));
+    }
+    static void rev(int[] x){
+//        int[] a=new int[x.length];
+        int s=0,e=x.length-1;
+        while(s<e){
+            int temp=x[s];
+            x[s]=x[e];
+            x[e]=temp;
+            s++;e--;
+        }
     }
 }
