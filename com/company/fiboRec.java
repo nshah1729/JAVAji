56b11e6a25727a1907497625eb471b56c1ecba0c
nishantshah <nishantshah1729@gmail.com>
2021-10-29 19:36:07 +0530

    commit JAVA

--- com/company/fiboRec.java
+++ com/company/fiboRec.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class fiboRec {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        System.out.println(fibo(n));
+        int n=fibo(4);
+        System.out.println(n);
     }
     static int fibo(int n){
 
