a31b16138f545c2c3d462736235d09f54ac35f73
nishantshah <nishantshah1729@gmail.com>
2021-10-31 08:09:37 +0530

    commit JAVA

--- com/company/BSFibo.java
+++ com/company/BSFibo.java
@@ -6,7 +6,8 @@ public class BSFibo {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int[] a={1,2,3,7,9,11,23,45};
-        System.out.println(BS(a,11,0,8));
+        int ans=BS(a,12,0,8);
+        System.out.println(ans);
     }
     static int BS(int[] arr,int target,int s,int e){
         if(s>e)return -1;
