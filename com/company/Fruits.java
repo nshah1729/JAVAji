b67afa2bb2f8f92a226b2198950fbb9a5f921ed1
nishantshah <nishantshah1729@gmail.com>
2021-12-03 09:45:52 +0530

    commit JAVA

--- com/company/Fruits.java
+++ com/company/Fruits.java
@@ -1,4 +1,10 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Fruits {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        
+    }
 }
