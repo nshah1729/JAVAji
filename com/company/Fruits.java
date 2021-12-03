45db4017cf2a9ffea059d11a3f5aaaf4bd9a07e7
nishantshah <nishantshah1729@gmail.com>
2021-12-03 18:39:17 +0530

    commit JAVA

--- com/company/Fruits.java
+++ com/company/Fruits.java
@@ -1,19 +1,38 @@
 package com.company;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Fruits {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int a=10,b=20;
-        System.out.println(a+" | "+ b);
-        swap(a,b);
-        System.out.println(a+" | "+ b);
-    }
-    static void swap(int a,int b){
-        int temp;
-        temp=a;
-        a=b;
-        b=temp;
+        Scanner sc = new Scanner(System.in);
+        // 2D ARRAY
+        int[][] ros = new int[3][3];
+
+        //INPUT:
+        //==================
+        //For Rows
+        for (int row = 0; row < ros.length; row++) {
+
+            //For Columns
+            for (int col = 0; col < ros[row].length; col++) {
+                ros[row][col] = sc.nextInt();
+            }
+        }
+//        System.out.println(Arrays.toString(ros));
+        for (int row = 0; row < ros.length; row++) {
+            System.out.println(Arrays.toString(ros[row]));
+        }
+        // For Output
+        // For Rows
+        for (int row = 0; row < ros.length; row++) {
+
+            //For Columns
+            for (int col = 0; col < ros[row].length; col++) {
+                System.out.print(ros[row][col] + " ");
+            }
+            System.out.println();
+//        }
+        }
     }
 }
---
+++ com/company/List.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class List {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+       int[] a=new int[5];
+        for (int i = 0; i <a.length; i++) {
+            a[i]= sc.nextInt();
+        }
+       rev(a);
+        System.out.println(Arrays.toString(a));
+    }
+
+    static void rev(int[] a) {
+        int s=0,e=a.length-1;
+        while(s<e){
+            int temp=a[s];
+            a[s]=a[e];
+            a[e]=temp;
+            s++;e--;
+        }
+    }
+}
