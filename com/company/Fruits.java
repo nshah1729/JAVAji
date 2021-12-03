5e560b1e6b647dab52c70571b61bfe37bb33595b
nishantshah <nishantshah1729@gmail.com>
2021-12-03 11:03:04 +0530

    commit JAVA

--- com/company/Fruits.java
+++ com/company/Fruits.java
@@ -5,6 +5,15 @@ import java.util.Scanner;
 public class Fruits {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        
+        int a=10,b=20;
+        System.out.println(a+" | "+ b);
+        swap(a,b);
+        System.out.println(a+" | "+ b);
+    }
+    static void swap(int a,int b){
+        int temp;
+        temp=a;
+        a=b;
+        b=temp;
     }
 }
