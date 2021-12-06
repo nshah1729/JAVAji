0fbff5dbc254162c7758344ab3b7d1c110026547
nishantshah <nishantshah1729@gmail.com>
2021-12-06 17:58:34 +0530

    commit JAVA

--- com/company/SelectionSort.java
+++ com/company/SelectionSort.java
@@ -7,10 +7,10 @@ public class SelectionSort {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int[] a={634,234635,754,785,786,689,790,47};
-        select(a);
+        selection(a);
         System.out.println(Arrays.toString(a));
     }
-    static void select(int[] a){
+    static void selection(int[] a){
 
     }
 }
