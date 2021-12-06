e4f0c7685ba096924d675471eaf80389cd0a182a
nishantshah <nishantshah1729@gmail.com>
2021-12-06 11:22:16 +0530

    commit JAVA

--- com/company/BubbleSort.java
+++ com/company/BubbleSort.java
@@ -6,9 +6,12 @@ import java.util.*;
 public class BubbleSort {
     public static void main(String[] args) {
        Scanner sc=new Scanner(System.in);
-        int[] a={};
+        int[] a={634,234635,754,785,786,689,790,47};
+        int[] x={634,234635,754,785,786,689,790,47};
         bubble(a);
+        bubblee(x);
         System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(x));
     }
     static void bubble(int[] x){
         boolean isSwapped;
@@ -27,4 +30,24 @@ public class BubbleSort {
             if(!isSwapped)break;
         }
     }
+
+
+    static void bubblee(int[] x){
+        boolean isSwapped;
+        for (int i = x.length-1; i >= 0; i--) {
+            isSwapped=false;
+            for (int j = 1; j <=i; j++) {
+
+                //Swapping
+                if(x[j]<x[j-1]){
+                    int temp=x[j];
+                    x[j]=x[j-1];
+                    x[j-1]=temp;
+                    isSwapped=true;
+                }
+            }
+            if(!isSwapped)break;
+        }
+    }
+
 }
