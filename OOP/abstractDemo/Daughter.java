2d974d2c1815635a05cdba2492da29f071f3268e
nishantshah <nishantshah1729@gmail.com>
2021-12-12 13:06:33 +0530

    commit JAVA

---
+++ OOP/abstractDemo/Daughter.java
@@ -0,0 +1,13 @@
+package OOP.abstractDemo;
+
+public class Daughter extends Parent{
+    @Override
+    void career(String Choice) {
+        System.out.println("I am going to be a "+ Choice);
+    }
+
+    @Override
+    void car(String name) {
+        System.out.println("I am going to buy a "+ name);
+    }
+}
