978a31305c4a4250b1eebe9587bf122170830062
nishantshah <nishantshah1729@gmail.com>
2021-12-20 13:33:42 +0530

    commit JAVA

---
+++ com/DSA/Test.java
@@ -0,0 +1,9 @@
+package com.DSA;
+import java.util.*;
+public class Test {
+    public static void main(String[] args) {
+        int n=19;
+        System.out.println(n.toCharArray());
+        if (3>1);
+    }
+}
