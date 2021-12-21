3edccf465d906a6082d4595287b1c52edcab61c8
nishantshah <nishantshah1729@gmail.com>
2021-12-21 21:31:02 +0530

    commit JAVA

--- com/DSA/Test.java
+++ com/DSA/Test.java
@@ -2,8 +2,14 @@ package com.DSA;
 import java.util.*;
 public class Test {
     public static void main(String[] args) {
-        int n=19;
-        System.out.println(n.toCharArray());
-        if (3>1);
+        String p="Nishant";
+        String q="Nishant";
+        String x=new String("Nishant");
+        String y=new String("Nishant");
+        String a=new String("Nishant");
+        String b=new String("Nishant");
+        System.out.println(a.equals(b));
+        System.out.println(p==q);
+        System.out.println(x==y);
     }
 }
