095064d2411fdfd504ad6a4a3104b2ba1b421277
nishantshah <nishantshah1729@gmail.com>
2021-12-24 10:05:37 +0530

    commit JAVA

---
+++ com/DSA/PrettryPrint.java
@@ -0,0 +1,9 @@
+package com.DSA;
+
+public class PrettryPrint {
+    public static void main(String[] args) {
+        float n=12.3244f;
+//        System.out.printf("Formatted is: %.2f",n);
+        System.out.printf("Hello my name is %s and I am an %s","Nishant","AdhamJeev");
+    }
+}
