0189685a96fa9801233cb1eb24bbc893e4c3157c
nishantshah <nishantshah1729@gmail.com>
2021-12-29 14:58:44 +0530

    commit JAVA

---
+++ com/DSA/Bitwise/ConvertAtoBNoOfBitsChanged.java
@@ -0,0 +1,26 @@
+package com.DSA.Bitwise;
+
+//In converting a-->b, we need to find how many bits are to be changed:
+
+import java.util.Scanner;
+
+public class ConvertAtoBNoOfBitsChanged {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a: ");
+        int a= sc.nextInt();
+        System.out.print("Enter b: ");
+        int b= sc.nextInt();
+        System.out.println("No of bits required to be changed is "+noOfBitsChanged(a,b));
+    }
+
+    private static int noOfBitsChanged(int a, int b) {
+        int xor_value=a^b;
+        int count=0;
+        while (xor_value>0){
+            if((xor_value&1)==1)count++;
+            xor_value=xor_value>>1;
+        }
+        return count;
+    }
+}
