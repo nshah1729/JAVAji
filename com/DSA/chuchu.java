7d4af021739caee638d582bb6662dc80631d8e5b
nishantshah <nishantshah1729@gmail.com>
2022-01-05 14:58:34 +0530

    commit JAVA

---
+++ com/DSA/chuchu.java
@@ -0,0 +1,17 @@
+package com.DSA;
+
+import java.util.Arrays;
+
+public class chuchu {
+    public static void main(String[] args) {
+        int[] a = {1, 1, 2,5, 3, 3, 4, 4, 5, 6};
+
+        for (int i = 0; i < a.length; i++) {
+            int count = 0;
+            for (int j = 0; j < a.length; j++) {
+                if (a[i] == a[j])count++;
+            }
+            if(count==1) System.out.print(a[i]+" ");
+        }
+    }
+}
