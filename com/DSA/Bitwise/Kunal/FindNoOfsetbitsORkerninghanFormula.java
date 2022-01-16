e64a7cfa14533f00e84985435a3b285161373426
nishantshah <nishantshah1729@gmail.com>
2022-01-16 21:31:16 +0530

    commit JAVA

--- com/DSA/Bitwise/Kunal/FindNoOfsetbitsORkerninghanFormula.java
+++ com/DSA/Bitwise/Kunal/FindNoOfsetbitsORkerninghanFormula.java
@@ -8,6 +8,7 @@ public class FindNoOfsetbitsORkerninghanFormula {
         System.out.println("Enter the number");
         int n= sc.nextInt();
         find(n);
+        findans(n);
     }
     //Kerninghan's Formula:O(logn)
     private static void find(int n) {
@@ -15,6 +16,15 @@ public class FindNoOfsetbitsORkerninghanFormula {
         while(n>0){
             n-=(n&-n);//Clearing the RSB
             //We can also do n=n&(n-1) instead of n-=(n&-n)
+            ans++;
+        }
+        System.out.println(ans);
+    }
+    //Method 2:
+    private static void findans(int n) {
+        int ans=0;
+        while(n>0){
+            n=n&(n-1);
             ans++;
         }
         System.out.println(ans);
---
+++ com/DSA/Bitwise/Kunal/FindXORfromZeroToN.java
@@ -0,0 +1,21 @@
+package com.DSA.Bitwise.Kunal;
+
+import java.util.Scanner;
+
+//If n=5...then output is value of 1^2^3^4^5
+public class FindXORfromZeroToN {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n=sc.nextInt();
+        valueofXOR(n);
+    }
+
+    //Find a Repeating Pattern.
+    private static void valueofXOR(int n) {   //O(1);
+        if(n%4==0) System.out.println("The XOR value is: "+n);
+        if(n%4==1) System.out.println("The XOR value is: "+1);
+        if(n%4==2) System.out.println("The XOR value is: "+n+1);
+        if(n%4==3) System.out.println("The XOR value is: "+0);
+    }
+}
