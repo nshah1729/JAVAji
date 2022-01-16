bf216605d6db846c6f4717fa4bc89ddd3e6d6073
nishantshah <nishantshah1729@gmail.com>
2022-01-16 14:54:10 +0530

    commit JAVA

---
+++ com/DSA/Bitwise/Kunal/ElementsAreAppearingThriceExceptOne.java
@@ -0,0 +1,12 @@
+package com.DSA.Bitwise.Kunal;
+
+public class ElementsAreAppearingThriceExceptOne {
+    public static void main(String[] args) {
+        int[] a={1,1,1,2,2,2,3};
+        find(a);
+    }
+
+    private static void find(int[] a) {
+
+    }
+}
---
+++ com/DSA/Bitwise/Kunal/aPowerb.java
@@ -0,0 +1,18 @@
+package com.DSA.Bitwise.Kunal;
+
+public class aPowerb {
+    public static void main(String[] args) {
+        int a=6;
+        int b=3;
+        print(a,b);
+    }
+    public static void print(int a,int b){
+        int ans=1,base=a;
+        while(b>0){
+            if((b&1)==1)ans*=base;
+            base*=base;
+            b=b>>1;
+        }
+        System.out.println(ans);
+    }
+}
