c60f6629cb0f587d911514de223b37ded63e7459
nishantshah <nishantshah1729@gmail.com>
2022-01-18 21:19:01 +0530

    commit JAVA

---
+++ com/DSA/KK/MathForDSA/Factors.java
@@ -0,0 +1,18 @@
+package com.DSA.KK.MathForDSA;
+
+import java.util.Scanner;
+
+public class Factors {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n= sc.nextInt();
+        fac(n);
+    }
+
+    private static void fac(int n) {
+        for (int i = 1; i <= n; i++) {
+            if(n%i==0) System.out.print(i+" ");
+        }
+    }
+}
