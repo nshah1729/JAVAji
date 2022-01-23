d1d65b70b7dddf302838c48ce8402dca86ef01af
nishantshah <nishantshah1729@gmail.com>
2022-01-23 17:53:25 +0530

    commit JAVA

--- com/DSA/KK/MathForDSA/Factors.java
+++ com/DSA/KK/MathForDSA/Factors.java
@@ -1,5 +1,6 @@
 package com.DSA.KK.MathForDSA;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Factors {
@@ -7,12 +8,27 @@ public class Factors {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the number: ");
         int n= sc.nextInt();
-        fac(n);
+        fac2(n);
     }
 
     private static void fac(int n) {
         for (int i = 1; i <= n; i++) {
             if(n%i==0) System.out.print(i+" ");
+        }
+    }
+    private static void fac2(int n){
+        ArrayList<Integer> list=new ArrayList<>();
+        for (int i = 1; i*i <=n ; i++) {
+            if(n%i==0){
+                if(n/i==i) System.out.print(i+" ");
+                else{
+                    System.out.print(i+" ");
+                    list.add(n/i);
+                }
+            }
+        }
+        for (int i = list.size()-1; i >=0 ; i--) {
+            System.out.print(list.get(i)+" ");
         }
     }
 }
