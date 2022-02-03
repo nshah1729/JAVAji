f7ea6f2f301b0f7c13d07b56ae161bb372d0b05a
nishantshah <nishantshah1729@gmail.com>
2022-02-03 17:49:59 +0530

    commit JAVA

--- com/DSA/Sortingg/BubbleSort.java
+++ com/DSA/Sortingg/BubbleSort.java
@@ -5,16 +5,30 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main(String[] args) {
         int[] a={4,9,5,1,0};
-        System.out.println(Arrays.toString(bubble(a)));
+//        System.out.println(Arrays.toString(bubbleA(a)));
+        System.out.println(Arrays.toString(bubbleD(a)));
     }
-
-    private static int[] bubble(int[] a) {//O(n2)
-        for (int i = 0; i < a.length-1; i++) {     //For Outer Loop: O(n)
+    //Ascending Order:
+    private static int[] bubbleA(int[] a) {//O(n2)
+        for (int i = 0; i <= a.length-1; i++) {     //For Outer Loop: O(n)
             for (int j = 1; j < a.length-i; j++) { //For Inner Loop: O(n)
                 if(a[j]<a[j-1]){//O(1)
                     int temp=a[j];
                     a[j]=a[j-1];
                     a[j-1]=temp;
+                }
+            }
+        }
+        return a;
+    }
+    //Descending Order:
+    public static int[] bubbleD(int[] a){
+        for (int i = 0; i <=a.length-1; i++) {
+            for (int j = 1; j < a.length-i; j++) {
+                if(a[j]>a[j-1]){
+                    a[j]=a[j]^a[j-1];
+                    a[j-1]=a[j]^a[j-1];
+                    a[j]=a[j]^a[j-1];
                 }
             }
         }
