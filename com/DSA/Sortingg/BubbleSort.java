57f41812512795aa01c8b1de01e08fd3fb028248
nishantshah <nishantshah1729@gmail.com>
2022-02-03 14:53:01 +0530

    commit JAVA

--- com/DSA/Sortingg/BubbleSort.java
+++ com/DSA/Sortingg/BubbleSort.java
@@ -8,10 +8,10 @@ public class BubbleSort {
         System.out.println(Arrays.toString(bubble(a)));
     }
 
-    private static int[] bubble(int[] a) {
-        for (int i = 0; i < a.length-1; i++) {
-            for (int j = 1; j < a.length-i; j++) {
-                if(a[j]<a[j-1]){
+    private static int[] bubble(int[] a) {//O(n2)
+        for (int i = 0; i < a.length-1; i++) {     //For Outer Loop: O(n)
+            for (int j = 1; j < a.length-i; j++) { //For Inner Loop: O(n)
+                if(a[j]<a[j-1]){//O(1)
                     int temp=a[j];
                     a[j]=a[j-1];
                     a[j-1]=temp;
