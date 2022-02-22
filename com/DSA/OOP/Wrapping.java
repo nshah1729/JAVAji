76d61ef333f96c78f706b707b57f43bcfb257bbe
nishantshah <nishantshah1729@gmail.com>
2022-02-22 10:57:19 +0530

    commit JAVA

---
+++ com/DSA/OOP/Wrapping.java
@@ -0,0 +1,10 @@
+package com.DSA.OOP;
+
+public class Wrapping {
+    public static void main(String[] args) {
+        final int INCREASE=2;
+        String a="sef";
+        String b="sf";
+        a="aefs"+"fs";
+    }
+}
