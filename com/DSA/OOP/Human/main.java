e07229b543b1b00af3e0c85f7498136ea9e067b8
nishantshah <nishantshah1729@gmail.com>
2022-02-22 10:57:05 +0530

    commit JAVA

---
+++ com/DSA/OOP/Human/main.java
@@ -0,0 +1,10 @@
+package com.DSA.OOP.Human;
+
+public class main {
+    public static void main(String[] args) {
+        hooman nishant=new hooman("Nishant Shah",22);
+        hooman Krishna=new hooman("Vāsudev Krishnā",16);
+        System.out.println(hooman.population);
+        System.out.println(hooman.population);
+    }
+}
