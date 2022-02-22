5c45b35cd2b6e22da01de0258ed7519ad03083b8
nishantshah <nishantshah1729@gmail.com>
2022-02-22 10:56:51 +0530

    commit JAVA

---
+++ com/DSA/OOP/Properties/Polymorphism/Shapes.java
@@ -0,0 +1,11 @@
+package com.DSA.OOP.Properties.Polymorphism;
+
+public class Shapes {
+    public static void main(String[] args) {
+        int[] a={1,2,3,4};
+        for (int i = 0; i <=a.length; i++) {
+            System.out.println(a[i]);
+        }
+
+    }
+}
